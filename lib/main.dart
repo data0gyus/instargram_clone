bc817f44d65ed9a8960497978e123650d1481e0d
KimGyuSeok <dwjsk2008@gmail.com>
2025-01-19 21:16:19 +0900

    완성

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_instargram_clone/firebase_options.dart';
+import 'package:flutter_instargram_clone/src/app.dart';
 import 'package:flutter_instargram_clone/src/binding/init_binding.dart';
 import 'package:flutter_instargram_clone/src/root.dart';
 import 'package:get/get.dart';
@@ -28,7 +29,7 @@ class MyApp extends StatelessWidget {
             foregroundColor: Colors.black,
             elevation: 0.0,
           )),
-      home: const Root(),
+      home: const App(),
       initialBinding: InitBinding(),
     );
   }
